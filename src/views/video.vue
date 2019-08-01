97a4596cb4589a288b2897a885081de724b98d32
shi-zhonghua <shi996966@163.com>
2019-08-01 14:36:25 +0800

    [shizh] 视频时间显示

--- src/views/video.vue
+++ src/views/video.vue
@@ -1,13 +1,13 @@
 <template>
     <div style="width:600px; height:600px;">
-        <video-player  class="video-player vjs-custom-skin" ref="videoPlayer" :playsinline="true" :options="playerOptions"></video-player>
-        sdfsfssf
+        <video-player class="video-player vjs-custom-skin vjs-big-play-centered" id="videoPlayer" ref="videoPlayer" :playsinline="true" :options="playerOptions"></video-player>
     </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      videoUrl: "https://media.w3.org/2010/05/sintel/trailer.mp4",
       playerOptions: {
         playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
         autoplay: false, //如果true,浏览器准备好时开始回放。
@@ -19,11 +19,12 @@ export default {
         fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
         sources: [
           {
-            src: "https://media.w3.org/2010/05/sintel/trailer.mp4", // 路径
+            src: "https://media.w3.org/2010/05/sintel/trailer.mp4", // 视频路径
             type: "video/mp4" // 类型
           }
         ],
-        poster: "http://imsapi.yueqizhixiang.com/uploads/images/leqizhixiang/shangjiadianpu/zixun2/1564392065070.jpg", //你的封面地址
+        poster:
+          "http://imsapi.yueqizhixiang.com/uploads/images/leqizhixiang/shangjiadianpu/zixun2/1564392065070.jpg", //你的封面地址
         // width: document.documentElement.clientWidth,
         notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
         controlBar: {
@@ -34,16 +35,28 @@ export default {
         }
       }
     };
+  },
+  mounted() {
+      // 外部加载视频路径
+    //   this.playerOptions.sources[0].src=this.videoUrl
   }
 };
 </script>
 <style lang="less" >
-.video-js .vjs-big-play-button{
-    // background: #f0f;
-    position: absolute;
-    top:40%;
-    left:45%;
-    margin: -0.75rem -1.5rem;
+//  vjs-big-play-centered  播放按钮居中 添加这个类名
+.video-js .vjs-big-play-button {
+  //播放按钮样式
+  height: 2em;
+  width: 2em;
+  line-height: 2em;
+  border-radius: 1em;
+}
+// 显示视频总时长，当前播放时间  重点、重点、重点
+.video-js .vjs-time-control {
+  display: block;
+}
+.video-js .vjs-remaining-time {
+  display: none;
 }
 </style>
 
