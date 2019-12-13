1ed201f3f30ea98d8b84ece9473bb2342c20f206
shi-zhonghua <shi996966@163.com>
2019-12-13 10:53:05 +0800

    [shizh] 创建模板

---
+++ src/views/dev_index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    },
+    components:{
+
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    filters:{
+
+    }
+
+    
+    
+}
+
+</script>
+<style lang='less' scoped>
+</style>
